fa53de1b991a98f7c7c1b754f810a0d1e88f2459
Hyunmo Ryang <hyunmo@live.unc.edu>
2021-11-30 00:02:32 -0500

    all DONE AS;LDKGH

--- server.js
+++ server.js
@@ -45,14 +45,14 @@ app.get("/app/user/:id", (req, res) => {
 app.patch("/app/update/user/:id", (req, res) => {
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?");
 	const result = stmt.run(req.body.user, md5(req.body.pass), req.params.id);
-	res.status(200).json({"message":  result.changes + " record updated: ID " + req.params.id + "(200)"});
+	res.status(200).json({"message":  result.changes + " record updated: ID " + req.params.id + " (200)"});
 });
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 app.delete("/app/delete/user/:id", (req, res) => {
 	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?");
 	const result = stmt.run(req.params.id);
-	res.status(200).json({"message": result.changes + " record deleted: ID " + req.params.id + "(200)"});
+	res.status(200).json({"message": result.changes + " record deleted: ID " + req.params.id + " (200)"});
 });
 
 // Default response for any other request
